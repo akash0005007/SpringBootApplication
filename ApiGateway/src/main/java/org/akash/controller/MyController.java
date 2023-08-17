f1c5b6785b384953aaa4db169bcb202f4140578e
akash0005007 <akash0005007@gmail.com>
2023-08-17 17:43:28 +0530

    commit

--- ApiGateway/src/main/java/org/akash/controller/MyController.java
+++ ApiGateway/src/main/java/org/akash/controller/MyController.java
@@ -1,21 +1,41 @@
 package org.akash.controller;
 
-import org.akash.router.MyRouter;
+import org.akash.dto.AppUser;
+import org.akash.dto.AuthRequest;
+import org.akash.dto.CreateUserReq;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MyController {
-    MyRouter myRouter;
+    RestTemplate restTemplate;
 
     @Autowired
-    public MyController(MyRouter myRouter) {
-        this.myRouter = myRouter;
+    public MyController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/")
     public String availableToAll() {
-        return myRouter.myString();
+        String apiUrl = "http://localhost:8081/";
+        return restTemplate.getForObject(apiUrl, String.class);
     }
+    @PostMapping("/createUser")
+    public AppUser createUserFromAnotherService(@RequestBody CreateUserReq req) {
+        String createUserUrl = "http://localhost:8081/app-auth/createUser";
+        return restTemplate.postForObject(createUserUrl, req, AppUser.class);
+    }
+
+    @GetMapping("/generateToken")
+    public String generateToken(@RequestBody AuthRequest authRequest) {
+        String authUrl = "http://localhost:8081/app-auth/token";
+        ResponseEntity<String> response = restTemplate.postForEntity(authUrl, authRequest, String.class);
+        return response.getBody();
+    }
+
 }
---
+++ ApiGateway/src/main/java/org/akash/dto/AppUser.java
@@ -0,0 +1,42 @@
+package org.akash.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+public class AppUser {
+    private int id;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String userRole;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
